443a65a97a1f82b6e118f9ad052ee878e5d7df1a
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-05-04 17:12:18 +0200

    feat: some more tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,18 +2,40 @@ import { describe, it } from 'vitest'
 import { Installer } from '../src'
 
 describe('index', () => {
-    it('should return a download list', async () => {
-        //const insta = new Installer({ url: 'https://piston-meta.mojang.com/v1/packages/376f460c35740f490082af166f4464d5a8eeaf04/1.20.6.json', version: '1.20.6' });
+    it('should install 1.8.9', async () => {
+        let lastProgress = 0
         const insta = new Installer({ url: 'https://piston-meta.mojang.com/v1/packages/d546f1707a3f2b7d034eece5ea2e311eda875787/1.8.9.json', version: '1.8.9', path: './.minecraft' });
-            await insta.install({
-                onError() {},
-                onFinish() {
-                    console.log('done')
-                },
-                onProgress(prog) {
-                    console.log(`Progress: ${prog}%`)
-                },
-                onStart() {}
-            })
+        await insta.install({
+            onError() {},
+            onFinish() {
+                console.log('done')
+            },
+            onProgress(prog) {
+                const rounded = Math.round(prog)
+                if (rounded !== lastProgress) {
+                    console.log(`Progress: ${rounded}%`)
+                    lastProgress = rounded
+                }
+            },
+            onStart() {}
+        })
+    })
+    it('should install 1.20.6', async () => {
+        let lastProgress = 0
+        const insta = new Installer({ url: 'https://piston-meta.mojang.com/v1/packages/376f460c35740f490082af166f4464d5a8eeaf04/1.20.6.json', version: '1.20.6', path: './.minecraft' });
+        await insta.install({
+            onError() {},
+            onFinish() {
+                console.log('done')
+            },
+            onProgress(prog) {
+                const rounded = Math.round(prog)
+                if (rounded !== lastProgress) {
+                    console.log(`Progress: ${rounded}%`)
+                    lastProgress = rounded
+                }
+            },
+            onStart() {}
+        })
     })
 })
